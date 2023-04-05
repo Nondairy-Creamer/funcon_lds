38c83cb29ddeb2e4e3837a824e73d050c02eb848
Matt Creamer <matthew.s.creamer@gmail.com>
2023-04-04 20:00:14 -0400

    Bug fixing on the converted MATLAB code, appears to be working

--- matlab_implementation.py
+++ matlab_implementation.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.stats as ss
 
 
-def sampleLDSgauss(mm, nT, uu):
+def sampleLDSgauss(mm, nT, uu, rng=np.random.default_rng()):
     # [yy,zz,yin,zin] = sampleLDSgauss(mm,nT,uu)
     #
     # Sample data from a latent LDS-Bernoulli model
@@ -31,20 +31,18 @@ def sampleLDSgauss(mm, nT, uu):
     zz = np.zeros((nz, nT))
     yy = np.zeros((ny, nT))
 
-    rng = np.random.default_rng(0)
-
     # Process inputs
-    zin = np.concatenate((np.zeros((nz, 1)), mm['B'] @ uu[:, 2:]), axis=1)  # additive intput to latents
+    zin = np.concatenate((np.zeros((nz, 1)), mm['B'] @ uu[:, 1:]), axis=1)  # additive intput to latents
     yin = mm['D'] @ uu  # additive intput to observations
 
     # Sample data for first time bin
-    zz[:, 1] = rng.multivariate_normal(np.zeros((1,nz)), mm['Q0']).T  # 1st latent (Note: no input in 1st time bin)
-    yy[:, 1] = mm['C'] @ zz[:, 1] + yin[:, 1] + rng.multivariate_normal(np.zeros(1,ny), mm['R']).T  # 1st observation
+    zz[:, 0] = rng.multivariate_normal(np.zeros(nz), mm['Q0'])  # 1st latent (Note: no input in 1st time bin)
+    yy[:, 0] = mm['C'] @ zz[:, 0] + yin[:, 0] + rng.multivariate_normal(np.zeros(ny), mm['R'])  # 1st observation
 
     # Sample data for remaining bins
-    for jj in range(2, nT):
-        zz[:, jj] = mm['A'] @ zz[:, jj-1] + zin[:, jj] + rng.multivariate_normal(np.zeros(1, nz), mm['Q']).T  # latents
-        yy[:, jj] = mm['C'] @ zz[:, jj] + yin[:, jj] + rng.multivariate_normal(np.zeros(1, ny), mm.R).T  # observations
+    for jj in range(1, nT):
+        zz[:, jj] = mm['A'] @ zz[:, jj-1] + zin[:, jj] + rng.multivariate_normal(np.zeros(nz), mm['Q'])  # latents
+        yy[:, jj] = mm['C'] @ zz[:, jj] + yin[:, jj] + rng.multivariate_normal(np.zeros(ny), mm['R'])  # observations
 
     return yy, zz, yin, zin
 
@@ -90,7 +88,7 @@ def runKalmanSmooth(yy, uu, mm):
     nT = yy.shape[1]  # number of time bins
 
     # pre-compute C'@inv(R) and C'@inv(R)@C
-    CtRinv = mm['C'].T / mm['R']
+    CtRinv = np.linalg.solve(mm['R'].T, mm['C']).T
     CtRinvC = CtRinv @ mm['C']
 
     # check if input-latent matrix is provided
@@ -110,7 +108,7 @@ def runKalmanSmooth(yy, uu, mm):
     zzcov = np.zeros((nz, nz, nT))  # marginal cov of zz(t)
     munext = np.zeros((nz, nT))  # prior mean for next step:  E[ z(t) | y(1:t-1)]
     Pnext = np.zeros((nz, nz, nT))  # prior cov for next step: cov[ z(t) | y(1:t-1)]
-    logcy = np.zeros((1, nT))  # store conditionals P(y(t) | y(1:t))
+    logcy = np.zeros(nT)  # store conditionals P(y(t) | y(1:t))
 
     # ============================================
     # Kalman Filter (forward pass through data)
@@ -119,18 +117,18 @@ def runKalmanSmooth(yy, uu, mm):
     # process 1st time bin
     zzcov[:, :, 0] = np.linalg.inv(np.linalg.inv(mm['Q0']) + CtRinvC)
     zzmu[:, 0] = zzcov[:, :, 0] @ (CtRinv @ yyctr[:, 0])  # NOTE: no inputs in first time bin
-    logcy[0] = ss.multivariate_normal((mm['C'] @ zin[:, 0]).T, mm['C'] @ mm['Q0'] @ mm['C'].T + mm['R']).log(yyctr[:, 0].T)
+    logcy[0] = ss.multivariate_normal(mm['C'] @ zin[:, 0], mm['C'] @ mm['Q0'] @ mm['C'].T + mm['R']).logpdf(yyctr[:, 0])
 
-    for tt in range(1, nT + 1):
+    for tt in range(1, nT):
         # Step 1 (Predict):
-        Pnext[:, :, tt] = mm['A'] @ zzcov[:, :, tt-1] @ mm['A'].T + mm.Q # prior cov for time bin t
+        Pnext[:, :, tt] = mm['A'] @ zzcov[:, :, tt-1] @ mm['A'].T + mm['Q']  # prior cov for time bin t
         munext[:, tt] = mm['A'] @ zzmu[:, tt-1] + zin[:, tt]  # prior mean for time bin t
         # Step 2 (Update):
-        zzcov[:, :, tt] = np.linalg.inv(np.linalg.inv[Pnext[:, :, tt]] + CtRinvC)   # KF cov for time bin t
-        zzmu[:,tt] = zzcov[:, :, tt] @ (CtRinv @ yyctr[:, tt] + np.linalg.solve(Pnext[:, :, tt], munext[:, tt])) # KF mean
+        zzcov[:, :, tt] = np.linalg.inv(np.linalg.inv(Pnext[:, :, tt]) + CtRinvC)   # KF cov for time bin t
+        zzmu[:, tt] = zzcov[:, :, tt] @ (CtRinv @ yyctr[:, tt] + np.linalg.solve(Pnext[:, :, tt], munext[:, tt]))  # KF mean
 
         # compute log P(y_t | y_{1:t-1})
-        logcy[tt] = ss.multivariate_normal((mm['C'] @ munext[:, tt]).T, mm['C'] @ Pnext[:, :, tt] @ mm['C'].T + mm['R']).log(yyctr[:, tt].T)
+        logcy[tt] = ss.multivariate_normal(mm['C'] @ munext[:, tt], mm['C'] @ Pnext[:, :, tt] @ mm['C'].T + mm['R']).logpdf(yyctr[:, tt])
 
     # compute marginal log-likelihood P(y | theta)
     logli = np.sum(logcy)
@@ -142,11 +140,11 @@ def runKalmanSmooth(yy, uu, mm):
     zzcov_offdiag1 = np.zeros((nz, nz, nT - 1))
 
     # Pass backwards, updating mean and covariance with info from the future
-    for tt in reversed(range(1, nT)):
-        Jt = (zzcov[:, :, tt] @ mm['A'].T) / Pnext[:, :, tt+1]  # matrix we need
-        zzcov[:, :, tt] = zzcov[:, :, tt] + Jt @ (zzcov[:, :, tt+1] - Pnext[:, :, tt+1]) @ Jt.T # update cov
+    for tt in reversed(range(0, nT - 1)):
+        Jt = np.linalg.solve(Pnext[:, :, tt+1].T, mm['A'] @ zzcov[:, :, tt].T).T  # matrix we need
+        zzcov[:, :, tt] = zzcov[:, :, tt] + Jt @ (zzcov[:, :, tt+1] - Pnext[:, :, tt+1]) @ Jt.T  # update cov
         zzmu[:, tt] = zzmu[:, tt] + Jt @ (zzmu[:, tt+1] - munext[:, tt+1])  # update mean
-        zzcov_offdiag1[:, :, tt] = Jt @ zzcov[:, :, tt + 1]
+        zzcov_offdiag1[:, :, tt] = Jt @ zzcov[:, :, tt+1]
 
     return zzmu, logli, zzcov, zzcov_offdiag1
 
@@ -184,114 +182,42 @@ def runEM_LDSgaussian(yy, mm, uu, optsEM):
 
     # Set EM optimization params if necessary
     # Set up variables for EM
-    logEvTrace = np.zeros((optsEM.maxiter,1)) # trace of log-likelihood
-    logpPrev = -np.inf # prev value of log-likelihood
-    dlogp = np.inf # change in log-li
-    jj = 0 # counter
+    logEvTrace = np.zeros((optsEM['maxiter']))  # trace of log-likelihood
+    logpPrev = -np.inf  # prev value of log-likelihood
+    dlogp = np.inf  # change in log-li
+    jj = 0  # counter
 
     while (jj < optsEM['maxiter']) and (dlogp > optsEM['dlogptol']):
-        jj += 1 # iteration counter
-
         # --- run E step  -------
         zzmu, logp, zzcov, zzcov_d1 = runKalmanSmooth(yy, uu, mm)
         logEvTrace[jj] = logp
 
-        dlogp = logp - logpPrev # change in log-likelihood
-        logpPrev = logp # update previous log-likelihood
+        dlogp = logp - logpPrev  # change in log-likelihood
+        logpPrev = logp  # update previous log-likelihood
 
         # Stop if LL decreased (for debugging purposes)
         if dlogp < -1e-3:
             Warning('EM iter #d (logEv = #.1f): LOG-EV DECREASED (dlogEv = #-.3g)', jj, logp, dlogp)
 
         # --- run M step  -------
-        if uu is None:
-            mm = runMstep_LDSgaussian(yy, mm, zzmu, zzcov, zzcov_d1, optsEM) # if no inputs
-        else:
-            mm = runMstep_LDSgaussian_wInputs(yy, uu, mm, zzmu, zzcov, zzcov_d1, optsEM)
+        mm = runMstep_LDSgaussian_wInputs(yy, uu, mm, zzmu, zzcov, zzcov_d1, optsEM)
 
         # ---  Display progress ----
-        if np.mod(jj, optsEM.display) == 0:
-            print('--- EM iter #d: logEv= #.3f ---\n', jj,logp)
+        if np.mod(jj, optsEM['display']) == 0:
+            print('--- EM iter', jj, ': logEv=', logp, '---')
+
+        jj += 1  # iteration counter
 
     # ---- Report EM termination stats ----------
-    if optsEM.display < np.inf:
-        if dlogp<optsEM.dlogptol:
-            print('EM finished in #d iters (dlogli=#f)\n', jj, dlogp)
-            logEvTrace[jj+1:] = []
+    if optsEM['display'] < np.inf:
+        if dlogp < optsEM['dlogptol']:
+            print('EM finished in', jj, 'iters (dlogli=,', dlogp, ')')
         else:
-            print('EM stopped at MAXITERS=#d iters (dlogli=#f)\n',jj,dlogp)
+            print('EM stopped at MAXITERS=', jj, 'iters (dlogli=', dlogp, ')')
+
+    logEvTrace = np.delete(logEvTrace, np.where(logEvTrace == 0))
 
     return mm, logEvTrace
-
-
-def runMstep_LDSgaussian(yy, mm, zzmu, zzcov, zzcov_d1, optsEM):
-    # mm = runMstep_LDSgaussian(yy,mm,ss,zzmu,zzcov,zzcov_d1,optsEM)
-    #
-    # Run M-step updates for LDS-Gaussian model
-    #
-    # Inputs
-    # =======
-    #     yy [ny x T] - Bernoulli observations- design matrix
-    #     mm [struct] - model structure with fields
-    #           .A [nz x nz] - dynamics matrix
-    #           .B [nz x ns] - input matrix (optional)
-    #           .C [ny x nz] - latents-to-observations matrix
-    #           .D [ny x ns] - input-to-observations matrix (optional)
-    #           .Q [nz x nz] - latent noise covariance
-    #           .Q0 [ny x ny] - latent noise covariance
-    #    zzmu [nz x T]     - posterior mean of latents
-    #   zzcov [nz*T x nz*T] - sparse Hessian for latents around zzmap
-    #
-    # Output
-    # =======
-    #  mmnew - new model struct with updated parameters
-
-    # Extract sizes
-    nt = zzmu.shape[1]     # number of time bins
-
-    # =============== Update dynamics parameters ==============
-    if optsEM.update.Dynam:
-        # Compute sufficient statistics
-        Mz1 = np.sum(zzcov[:, :, :nt], dim=2) + zzmu[:, :nt] @ zzmu[:, :nt].T # E[zz@zz'] for 1 to T-1
-        Mz2 = np.sum(zzcov[:, :, 2:nt+1], dim=2) + zzmu[:, 2:nt+1] @ zzmu[:, 2:nt+1].T # E[zz@zz'] for 2 to T
-        Mz12 = np.sum(zzcov_d1, dim=2) + (zzmu[:, :nt] @ zzmu[:, 2:nt+1].T)   # E[zz_t@zz_{t+1}'] (above-diag)
-
-        # update dynamics matrix A
-        if optsEM['update']['A']:
-            Anew = Mz12.T / Mz1
-            mm['A'] = Anew
-
-        # Update noise covariance Q
-        if optsEM['update']['Q']:
-            Qnew = (Mz2 + mm['A'] @ Mz1 @ mm['A'].T - mm['A'] @ Mz12 - Mz12.T @ mm['A'].T) / (nt - 1)
-            Qnew = (Qnew + Qnew.T) / 2  # symmetrize (to avoid numerical issues)
-            mm.Q = Qnew
-
-    # =============== Update observation parameters ==============
-    if optsEM.update.Obs:
-        # Compute sufficient statistics
-        if optsEM.update.Dynam:
-            Mz = Mz1 + zzcov[:, :, nt] + zzmu[:, nt] @ zzmu[:, nt].T  # re-use Mz1 if possible
-        else:
-            Mz = np.sum(zzcov, dim=2) + zzmu @ zzmu.T # E[zz@zz']
-
-        Mzy = zzmu @ yy.T # E[zz@yy']
-
-        # update obs matrix C
-        if optsEM.update.C:
-            Cnew = Mzy.T / Mz
-            mm.C = Cnew
-
-
-        # update obs noise covariance R
-        if optsEM['update']['R']:
-            My = yy @ yy.T  # compute suff stat E[yy@yy']
-
-            Rnew = (My + mm['C'] @ Mz @ mm['C'].T - mm['C'] @ Mzy - Mzy.T @ mm['C'].T) / nt
-            # Rnew = (Rnew + Rnew')/2  # symmetrize
-            mm['R'] = Rnew
-
-    return mm
 
 
 def runMstep_LDSgaussian_wInputs(yy, uu, mm, zzmu, zzcov, zzcov_d1, optsEM):
@@ -324,36 +250,36 @@ def runMstep_LDSgaussian_wInputs(yy, uu, mm, zzmu, zzcov, zzcov_d1, optsEM):
     #  mmnew - new model struct with updated parameters
 
     # Extract sizes
-    nz = mm['A'].shape[0] # number of latents
+    nz = mm['A'].shape[0]  # number of latents
     nt = zzmu.shape[1]     # number of time bins
 
     # =============== Update dynamics parameters ==============
     # Compute sufficient statistics for latents
-    Mz1 = np.sum(zzcov[:, :, :nt], dim=2) + zzmu[:, :nt] @ zzmu[:, :nt].T  # E[zz@zz'] for 1 to T-1
-    Mz2 = np.sum(zzcov[:, :, 2:nt+1], dim=2) + zzmu[:, 2:nt+1] @ zzmu[:, 2:nt+1].T  # E[zz@zz'] for 2 to T
-    Mz12 = np.sum(zzcov_d1, dim=2) + (zzmu[:,1:nt-1] @ zzmu[:, 2:nt+1].T)  # E[zz_t@zz_{t+1}'] (above-diag)
+    Mz1 = np.sum(zzcov[:, :, :-1], axis=2) + zzmu[:, :-1] @ zzmu[:, :-1].T  # E[zz@zz'] for 1 to T-1
+    Mz2 = np.sum(zzcov[:, :, 1:], axis=2) + zzmu[:, 1:] @ zzmu[:, 1:].T  # E[zz@zz'] for 2 to T
+    Mz12 = np.sum(zzcov_d1, axis=2) + (zzmu[:, :-1] @ zzmu[:, 1:].T)  # E[zz_t@zz_{t+1}'] (above-diag)
 
     # Compute sufficient statistics for inputs x latents
-    Mu = uu[:, 2:nt+1] @ uu[:, 2:nt+1].T  # E[uu@uu'] for 2 to T
-    Muz2 = uu[:, 2:nt+1] @ zzmu[:, 2:nt+1].T  # E[uu@zz'] for 2 to T
-    Muz21 = uu[:, 2:nt+1] @ zzmu[:, 1:nt].T  # E[uu_t@zz_{t-1} for 2 to T
+    Mu = uu[:, 1:] @ uu[:, 1:].T  # E[uu@uu'] for 2 to T
+    Muz2 = uu[:, 1:] @ zzmu[:, 1:].T  # E[uu@zz'] for 2 to T
+    Muz21 = uu[:, 1:] @ zzmu[:, :-1].T  # E[uu_t@zz_{t-1} for 2 to T
 
     # update dynamics matrix A & input matrix B
     if optsEM['update']['A'] and optsEM['update']['B']:
         # do a joint update for A and B
-        Mlin = np.concatenate((Mz12, Muz2), axis=0) # from linear terms
-        Mquad = np.concatenate((Mz1, Muz21.T, Muz21, Mu), axis=1) # from quadratic terms
-        ABnew = np.linalg.solve(Mlin.T, Mquad)  # new A and B from regression
-        mm['A'] = ABnew[:, :nz+1]  # new A
-        mm['B'] = ABnew[:, nz+1:]  # new B
+        Mlin = np.concatenate((Mz12, Muz2), axis=0)  # from linear terms
+        Mquad = np.block([[Mz1, Muz21.T], [Muz21, Mu]])  # from quadratic terms
+        ABnew = np.linalg.solve(Mquad.T, Mlin).T  # new A and B from regression
+        mm['A'] = ABnew[:, :nz]  # new A
+        mm['B'] = ABnew[:, nz:]  # new B
 
     elif optsEM['update']['A']:  # update dynamics matrix A only
-        Anew = (Mz12.T - mm['B'] @ Muz21) / Mz1  # new A
-        mm.A = Anew
+        Anew = np.linalg.solve(Mz1.T, Mz12 - Muz21.T @ mm['B'].T).T  # new A
+        mm['A'] = Anew
 
     elif optsEM['update']['B']:  # update input matrix B only
-        Bnew = (Muz2.T - mm['A'] @ Muz21.T) / Mu # new B
-        mm.B = Bnew
+        Bnew = np.linalg.solve(Mu.T, Muz2 - Muz21 @ mm['A'].T).T  # new B
+        mm['B'] = Bnew
 
 
     # Update noise covariance Q
@@ -365,9 +291,9 @@ def runMstep_LDSgaussian_wInputs(yy, uu, mm, zzmu, zzcov, zzcov_d1, optsEM):
 
     # =============== Update observation parameters ==============
     # Compute sufficient statistics
-    Mz = Mz1 + zzcov[:, :, nt] + zzmu[:, nt] @ zzmu[:, nt].T  # re-use Mz1 if possible
-    Mu = Mu + uu[:, 1] @ uu[:, 1].T  # reuse Mu
-    Muz = Muz2 + uu[:, 1] @ zzmu[:, 1].T # reuse Muz
+    Mz = Mz1 + zzcov[:, :, -1] + zzmu[:, -1, None] @ zzmu[:, -1, None].T  # re-use Mz1 if possible
+    Mu = Mu + uu[:, 0, None] @ uu[:, 0, None].T  # reuse Mu
+    Muz = Muz2 + uu[:, 0, None] @ zzmu[:, 0, None].T  # reuse Muz
 
     Mzy = zzmu @ yy.T  # E[zz@yy']
     Muy = uu @ yy.T   # E[uu@yy']
@@ -375,16 +301,16 @@ def runMstep_LDSgaussian_wInputs(yy, uu, mm, zzmu, zzcov, zzcov_d1, optsEM):
     # update obs matrix C & input matrix D
     if optsEM['update']['C'] and optsEM['update']['D']:
         # do a joint update to C and D
-        Mlin = np.concatenate((Mzy, Muy), dim=0)  # from linear terms
-        Mquad = np.concatenate((Mz, Muz.T, Muz, Mu), dim=1)  # from quadratic terms
-        CDnew = np.linalg.solve(Mlin.T, Mquad)  # new A and B from regression
-        mm['C'] = CDnew[:, :nz+1]  # new A
-        mm['D'] = CDnew[:, nz+1:]  # new B
+        Mlin = np.concatenate((Mzy, Muy), axis=0)  # from linear terms
+        Mquad = np.block([[Mz, Muz.T], [Muz, Mu]])  # from quadratic terms
+        CDnew = np.linalg.solve(Mquad.T, Mlin).T  # new A and B from regression
+        mm['C'] = CDnew[:, :nz]  # new A
+        mm['D'] = CDnew[:, nz:]  # new B
     elif optsEM['update']['C']:  # update C only
-        Cnew = (Mzy.T - mm['D'] @ Muz) / Mz  # new A
+        Cnew = np.linalg.solve(Mz.T, Mzy - Muz.T @ mm['D'].T).T  # new A
         mm['C'] = Cnew
     elif optsEM['update']['D']:  # update D only
-        Dnew = (Muy.T - mm['C'] @ Muz.T) / Mu  # new B
+        Dnew = np.linalg.solve(Mu.T, Muy - Muz @ mm['C'].T).T  # new B
         mm['D'] = Dnew
 
     # update obs noise covariance R
@@ -426,30 +352,18 @@ def alignLDSmodels(zz1, zztarg, mm1):
 
     # Transform inferred params to align with true params
     if 'A' in mmtarg.keys() and mmtarg['A'] is not None:
-        mmtarg['A'] = Walign @ mm1.A/Walign  # transform dynamics
+        mmtarg['A'] = Walign @ np.linalg.solve(Walign.T, mm1['A'].T).T  # transform dynamics
 
     if 'B' in mmtarg.keys() and mmtarg['B'] is not None:
-        mmtarg['B'] = Walign @ mm1.B     # transform input weights
+        mmtarg['B'] = Walign @ mm1['B']  # transform input weights
 
     if 'C' in mmtarg.keys() and mmtarg['C'] is not None:
-        mmtarg['C'] = mm1['C'] / Walign     # transform projection weights
+        mmtarg['C'] = np.linalg.solve(Walign.T, mm1['C'].T).T  # transform projection weights
 
     # transform latent noise covariances
-    mmtarg['Q'] = Walign @ mm1.Q @ Walign.T  # transform latent noise covariance
-    mmtarg['Q0'] = Walign @ mm1.Q0 @ Walign.T  # initial covariance
+    mmtarg['Q'] = Walign @ mm1['Q'] @ Walign.T  # transform latent noise covariance
+    mmtarg['Q0'] = Walign @ mm1['Q0'] @ Walign.T  # initial covariance
 
     return mmtarg
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- test_matlab.py
+++ test_matlab.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matlab_implementation as mi
 from scipy import linalg
+import scipy.io as sio
 
-
+# TODO: convert matricies from neurons x time to time x neurons
+# TODO: convert sampling so that the first time point comes from a step forward with A?
 # test_LDSgaussian_EMfitting.m
 #
 # Sample from a latent Gaussian linear dynamical system (LDS) model, then
@@ -10,16 +12,17 @@ from scipy import linalg
 
 # Basic equations:
 # -----------------
-# X_t = A*X_{t-1} + eps_x,  eps_x ~ N(0,Q)  # latents
-# Y_t = C*X_t + eps_y,      eps_y ~ N(0,R)  # observations
+# X_t = A@X_{t-1} + eps_x,  eps_x ~ N(0,Q)  # latents
+# Y_t = C@X_t + eps_y,      eps_y ~ N(0,R)  # observations
 #
 # With X_1 ~ N(0,Q0)    initial condition:
 
 # Set dimensions
-nz = 100  # dimensionality of latent z
-ny = 100  # dimensionality of observation y
-nu = 3  # dimensionality of external inputs
-nT = 5000 * 10  # number of time steps
+nz = 4  # dimensionality of latent z
+ny = 4  # dimensionality of observation y
+nu = 4  # dimensionality of external inputs
+nT = 1000 * 1  # number of time steps
+rng = np.random.default_rng(1)
 
 # Set model parameters
 # --------------------
@@ -28,27 +31,26 @@ nT = 5000 * 10  # number of time steps
 if nz == 2:
     # Use rotation matrix if nz = 2
     thet = np.pi / 25
-    A = 0.99 * np.array((np.cos(thet), np.sin(thet), -np.sin(thet), np.cos(thet)))
+    A = 0.99 * np.array(((np.cos(thet), np.sin(thet)), (-np.sin(thet), np.cos(thet))))
 else:
     # Generate random stable A
-    A = np.randn(nz)
-    u, s = np.linalg.eig(A, 'vector')  # get eigenvectors and eigenvals
+    A = rng.standard_normal((nz, nz))
+    s, u = np.linalg.eig(A)  # get eigenvectors and eigenvals
     s = s / np.max(np.abs(s)) * 0.98  # set largest eigenvalue to lie inside unit circle (enforcing stability)
-    s[np.real(s) < 0] = -s(np.real(s) < 0)  # set real parts to be positive (encouraging smoothness)
-    A = np.real(u * (np.diag(s) / u))  # reconstruct A
+    s[np.real(s) < 0] = -s[np.real(s) < 0]  # set real parts to be positive (encouraging smoothness)
+    A = np.real(u @ np.linalg.solve(u.T, np.diag(s)).T)  # reconstruct A
 
-rng = np.random.default_rng(0)
 # Set observation matrix C
-C = 0.5 * rng.standard_normal(ny, nz)  # loading weights
+C = 0.5 * rng.standard_normal((ny, nz))  # loading weights
 
 # Set input matrices B and D
 B = 0.5 * rng.standard_normal((nz, nu))  # weights from inputs to latents
 D = 0.5 * rng.standard_normal((ny, nu))  # weights from inputs to observed
 
 # Dynamics noise covariance
-Q = rng.standard_normal(nz)
-Q = 0.1 * (Q.T * Q + np.eye(nz))  # dynamics noise covariance
-R = np.diag(1 * np.standard_normal((ny, 1)) + 0.1)  # Y noise covariance
+Q = rng.standard_normal((nz, nz))
+Q = 0.1 * (Q.T @ Q + np.eye(nz))  # dynamics noise covariance
+R = np.diag(1 * rng.uniform(size=ny) + 0.1)  # Y noise covariance
 Q0 = 2 * np.eye(nz)  # Covariance for latent in first time step
 
 # Use discrete Lyapunov equation solver to compute asymptotic covariance
@@ -56,7 +58,7 @@ P = linalg.solve_discrete_lyapunov(A, Q)
 
 ## Sample data from LDS model
 
-uu = rng.standard_normal(nu, nT)  # external inputs
+uu = rng.standard_normal((nu, nT))  # external inputs
 
 mmtrue = {'A': A,
           'B': B,
@@ -67,12 +69,12 @@ mmtrue = {'A': A,
           'Q0': Q0,
           }
 
-yy, zz = mi.sampleLDSgauss(mmtrue, nT, uu)  # sample from model
+yy, zz = mi.sampleLDSgauss(mmtrue, nT, uu, rng)[:2]  # sample from model
 
 ## Compute latents and log-marginal likelihood given true params
 
 # Run Kalman Filter-Smoother to get posterior over latents given true data
-zzmutrue, loglitrue, zzcovtrue = mi.runKalmanSmooth(yy, uu, mmtrue)
+zzmutrue, loglitrue, zzcovtrue = mi.runKalmanSmooth(yy, uu, mmtrue)[:3]
 print('Log-evidence at true params: ' + str(loglitrue))
 
 ## Compute ML estimate for model params using EM
@@ -88,55 +90,49 @@ optsEM = {'maxiter': 250,  # maximum # of iterations
                      'D': True,
                      'Q': True,
                      'R': True,
+                     'Q0': True,
                      },
           }
 
 # Initialize fitting struct
-mm0 = {'A': A,
-       'B': B,
-       'C': C,
-       'D': D,
-       'Q': Q,
-       'R': R,
-       'Q0': Q0,
-       }
+mm0 = {}
 
-# make struct with initial params
-if optsEM['update']['A']:
-    mm0.A = A * 0.5 + rng.standard_normal(nz) * 0.1 # initial A param
+if optsEM['update']['A']: mm0['A'] = 0.5 * A + 0.1 * rng.standard_normal((nz, nz))
+if optsEM['update']['C']: mm0['C'] = 0.9 * C + 0.1 * rng.standard_normal((ny, nz))
+if optsEM['update']['Q']: mm0['Q'] = 1.33 * Q
+if optsEM['update']['R']: mm0['R'] = 1.5 * R
+if optsEM['update']['B']: mm0['B'] = 0.5 * B
+if optsEM['update']['D']: mm0['D'] = 0.5 * D
+if optsEM['update']['Q0']: mm0['Q0'] = Q0
 
-if optsEM['update']['C']:
-    mm0.C = C * .9 + rng.standard_normal(ny, nz) * 0.1 # initial C param
-
-if optsEM['update']['Q']:
-    mm0.Q = Q * 1.33 # initial Q param
-
-if optsEM['update']['R']:
-    mm0.R = R * 1.5 # initial R param
-
-if optsEM['update']['B']:
-    mm0.B = B * 0.5 # initial B param
-
-if optsEM['update']['D']:
-    mm0.D = D * 0.5 # initial D param
+optsEM['update']['Dynam'] = True
+optsEM['update']['Obs'] = True
 
 ## Run EM
-
 # Run EM inference for model parameters
+# save the generated data
+save_folder = '/home/mcreamer/Documents/data_sets/matlab_kalman_data.mat'
+save_dict = {'yy': yy,
+             'mm0': mm0,
+             'uu': uu,
+             'optsEM': optsEM,
+             }
+sio.savemat(save_folder, save_dict)
+
 mm1, logEvTrace = mi.runEM_LDSgaussian(yy, mm0, uu, optsEM)
 
 ## Examine fitted model
 
 # Compute posterior mean and cov of latents, and log-evidence at optimum
-zzmu1, logli1, zzcov1 = mi.runKalmanSmooth(yy, uu, mm1)
+zzmu1, logli1, zzcov1 = mi.runKalmanSmooth(yy, uu, mm1)[:3]
 
 # Align fitted model with true model (so we can compare params)
 mm1a = mi.alignLDSmodels(zzmu1, zzmutrue, mm1)
-zzmu1a, logli1a, zzcov1a = mi.runKalmanSmooth(yy, uu, mm1a) # recompute logli (just to make sure it hasn't changed)
+zzmu1a, logli1a, zzcov1a = mi.runKalmanSmooth(yy, uu, mm1a)[:3]  # recompute logli (just to make sure it hasn't changed)
 
 # Report whether optimization succeeded in finding a posible global optimum
-print('Log-evidence at true params: ' + str(loglitrue))
-print('Log-evidence at inferred params: ' + str(logli1))
+print('Log-evidence at true params:', loglitrue)
+print('Log-evidence at inferred params:', logli1)
 # Report if we found the global optimum
 if logli1 >= loglitrue:
     print('(found better optimum -- SUCCESS!)')
