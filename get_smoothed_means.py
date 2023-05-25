a50dcd5db0ee409abc8fc37b0b0dff365ff3eb02
Matt Creamer <matthew.s.creamer@gmail.com>
2023-05-25 19:43:28 -0400

    simplified parrallel smoothed code

--- get_smoothed_means.py
+++ get_smoothed_means.py
@@ -4,7 +4,12 @@ import pickle
 import loading_utilities as lu
 import inference_utilities as iu
 import os
+from mpi4py import MPI
+import numpy as np
 
+comm = MPI.COMM_WORLD
+cpu_id = comm.Get_rank()
+num_cpus = comm.Get_size()
 
 if len(sys.argv) == 2:
     model_folder = Path(sys.argv[1])
@@ -26,23 +31,65 @@ else:
 
     model_folder = Path(max_dir)
 
-# load in the model and the data
 model_path = model_folder / 'model_trained.pkl'
-data_path = model_folder / 'data.pkl'
-
 model_file = open(model_path, 'rb')
 model = pickle.load(model_file)
 model_file.close()
 
-data_file = open(data_path, 'rb')
-data = pickle.load(data_file)
-data_file.close()
+if cpu_id == 0:
+    # load in the model and the data
+    data_path = model_folder / 'data.pkl'
 
-emissions = data['emissions']
-inputs = data['inputs']
-cell_ids = data['cell_ids']
+    data_file = open(data_path, 'rb')
+    data = pickle.load(data_file)
+    data_file.close()
 
-smoothed_means = iu.parallel_smoother(model, emissions, inputs)
+    emissions_list = data['emissions']
+    inputs_list = data['inputs']
+    cell_ids = data['cell_ids']
 
-lu.save_run(model_folder.parent, smoothed_means=smoothed_means)
+    # convert the data to tensors
+    emissions, inputs = model.standardize_inputs(emissions_list, inputs_list)
 
+    # if not provided with an initial mean / cov calculate them
+    init_mean = model.estimate_init_mean(emissions)
+    init_cov = model.estimate_init_cov(emissions)
+
+    # package the data to be sent out to each of the children
+    # if there is more data than chilren, create groups of data to be sent to each
+    data_out = list(zip(emissions, inputs, init_mean, init_cov))
+    num_data = len(emissions_list)
+    chunk_size = int(np.ceil(num_data / num_cpus))
+    # split data out into a list of inputs
+    data_out = [data_out[i:i + chunk_size] for i in range(0, num_data, chunk_size)]
+else:
+    data_out = None
+
+print('Got to cpu:', cpu_id)
+data = iu.individual_scatter(data_out, root=0)
+smoothed_means = []
+
+for d in data:
+    this_emission = d[0]
+    this_input = d[1]
+    this_init_mean = d[2]
+    this_init_cov = d[3]
+    smoothed_means.append(
+        model.lgssm_smoother(this_emission, this_input, init_mean=this_init_mean, init_cov=this_init_cov)[3])
+
+print('Finished cpu:', cpu_id)
+
+smoothed_means = iu.individual_gather(smoothed_means, root=0)
+
+if cpu_id == 0:
+    print('Gathered')
+
+    smoothed_means_out = []
+    for i in smoothed_means:
+        for j in i:
+            smoothed_means_out.append(j)
+
+    smoothed_means = smoothed_means_out
+
+    lu.save_run(model_folder.parent, smoothed_means=smoothed_means)
+    print('saved')
--- inference_utilities.py
+++ inference_utilities.py
@@ -69,59 +69,6 @@ def solve_masked(A, b, mask):
     return x_hat
 
 
-def parallel_smoother(model, emissions_list, inputs_list, init_mean=None, init_cov=None):
-    comm = MPI.COMM_WORLD
-    cpu_id = comm.Get_rank()
-    num_cpus = comm.Get_size()
-
-    if cpu_id == 0:
-        # convert the data to tensors
-        emissions, inputs = model.standardize_inputs(emissions_list, inputs_list)
-
-        # if not provided with an initial mean / cov calculate them
-        if init_mean is None:
-            init_mean = model.estimate_init_mean(emissions)
-        else:
-            init_mean = [torch.tensor(i, device=model.device, dtype=model.dtype) for i in init_mean]
-
-        if init_cov is None:
-            init_cov = model.estimate_init_cov(emissions)
-        else:
-            init_cov = [torch.tensor(i, device=model.device, dtype=model.dtype) for i in init_cov]
-
-        # package the data to be sent out to each of the children
-        # if there is more data than chilren, create groups of data to be sent to each
-        data_out = list(zip(emissions, inputs, init_mean, init_cov))
-        num_data = len(emissions_list)
-        chunk_size = int(np.ceil(num_data / num_cpus))
-        # split data out into a list of inputs
-        data_out = [data_out[i:i + chunk_size] for i in range(0, num_data, chunk_size)]
-    else:
-        data_out = None
-
-    data = individual_scatter(data_out, root=0)
-    smoothed_means = []
-
-    for d in data:
-        this_emission = d[0]
-        this_input = d[1]
-        this_init_mean = d[2]
-        this_init_cov = d[3]
-        smoothed_means.append(model.lgssm_smoother(this_emission, this_input, init_mean=this_init_mean, init_cov=this_init_cov)[3])
-
-    smoothed_means = individual_gather(smoothed_means, root=0)
-
-    if cpu_id == 0:
-        smoothed_means_out = []
-        for i in smoothed_means:
-            for j in i:
-                smoothed_means_out.append(j)
-
-        smoothed_means = smoothed_means_out
-
-        return smoothed_means
-
-
 def fit_em(model, emissions_list, inputs_list, init_mean=None, init_cov=None, num_steps=10,
            save_folder='trained_models', save_every=10):
     comm = MPI.COMM_WORLD
@@ -153,8 +100,6 @@ def fit_em(model, emissions_list, inputs_list, init_mean=None, init_cov=None, nu
 
     start = time.time()
     for ep in range(num_steps):
-        model = comm.bcast(model, root=0)
-
         ll, smoothed_means = model.em_step(emissions, inputs, init_mean, init_cov, cpu_id=rank, num_cpus=size)
 
         if ll is None:
