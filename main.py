6608d913e8ee61c0029293278de58d78c91cacea
Matthew Creamer <matthew.s.creamer@gmail.com>
2023-07-22 16:09:12 -0400

    Updated paths to use pathlib

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import os
 from simple_slurm import Slurm
 from datetime import datetime, time
-
+from pathlib import Path
 
 if len(sys.argv) == 1:
     param_name = 'synthetic_test'
@@ -17,18 +17,19 @@ now = datetime.now()
 beginning_of_day = datetime.combine(now.date(), time(0))
 seconds_of_day = (now - beginning_of_day).seconds
 
-save_folder = 'trained_models/' + param_name + '/' + current_date + '_' + str(seconds_of_day)
+save_folder = 'trained_models' / Path(param_name) / Path(current_date + '_' + str(seconds_of_day))
 os.makedirs(save_folder)
 
 if 'slurm' in run_params.keys():
     # default values that should be the same for
-    slurm = Slurm(**run_params['slurm'], output=save_folder)
+    full_path = Path(__file__).parent.resolve()
+    slurm = Slurm(**run_params['slurm'], output=full_path / save_folder)
 
     run_command = ['module purge',
                    'module load anaconda3/2022.10',
                    'module load openmpi/gcc/4.1.2,',
                    'conda activate fast-mpi4py,',
-                   'srun python -uc \'import fit_data; fit_data.' + run_params['fit_file'] + '(\'' + param_name + '\',\'' + save_folder + '\')\'',
+                   'srun python -uc \"import fit_data; fit_data.' + run_params['fit_file'] + '(\'' + param_name + '\',\'' + save_folder + '\')\"',
                    ]
 
     slurm.sbatch('\n'.join(run_command))
