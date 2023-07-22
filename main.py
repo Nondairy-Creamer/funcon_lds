a997029adf39637d5bb591753dbe0987a98985dc
Matthew Creamer <matthew.s.creamer@gmail.com>
2023-07-22 18:02:00 -0400

    Made save path absolute

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime, time
 from pathlib import Path
 
 if len(sys.argv) == 1:
-    param_name = 'slurm_test'
+    param_name = 'synthetic_test'
 else:
     param_name = sys.argv[1]
 
@@ -17,12 +17,12 @@ now = datetime.now()
 beginning_of_day = datetime.combine(now.date(), time(0))
 seconds_of_day = (now - beginning_of_day).seconds
 
-save_folder = 'trained_models' / Path(param_name) / Path(current_date + '_' + str(seconds_of_day))
+full_path = Path(__file__).parent.resolve()
+save_folder = full_path / 'trained_models' / Path(param_name) / Path(current_date + '_' + str(seconds_of_day))
 os.makedirs(save_folder)
 
 if 'slurm' in run_params.keys():
     # default values that should be the same for
-    full_path = Path(__file__).parent.resolve()
     slurm = Slurm(**run_params['slurm'], output=full_path / save_folder / 'slurm_%A.out')
 
     run_command = ['module purge',
