504ba0db221741525c2cfff570f0a8c29877aa49
Matthew Creamer <matthew.s.creamer@gmail.com>
2023-07-22 21:20:50 -0400

    added MPI parallelization to main

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import os
 from simple_slurm import Slurm
 from datetime import datetime, time
 from pathlib import Path
+from mpi4py import MPI
+from mpi4py.util import pkl5
+
+comm = pkl5.Intracomm(MPI.COMM_WORLD)
+size = comm.Get_size()
+rank = comm.Get_rank()
+is_parallel = size > 1
 
 if len(sys.argv) == 1:
     # param_name = 'synthetic_test'
@@ -13,14 +20,18 @@ else:
     param_name = sys.argv[1]
 
 run_params = lu.get_run_params(param_name=param_name)
-current_date = datetime.today().strftime('%Y_%m_%d')
-now = datetime.now()
-beginning_of_day = datetime.combine(now.date(), time(0))
-seconds_of_day = (now - beginning_of_day).seconds
 
-full_path = Path(__file__).parent.resolve()
-save_folder = full_path / 'trained_models' / Path(param_name) / Path(current_date + '_' + str(seconds_of_day))
-os.makedirs(save_folder)
+if rank == 0:
+    current_date = datetime.today().strftime('%Y_%m_%d')
+    now = datetime.now()
+    beginning_of_day = datetime.combine(now.date(), time(0))
+    seconds_of_day = (now - beginning_of_day).seconds
+
+    full_path = Path(__file__).parent.resolve()
+    save_folder = full_path / 'trained_models' / Path(param_name) / Path(current_date + '_' + str(seconds_of_day))
+    os.makedirs(save_folder)
+else:
+    save_folder = None
 
 if 'slurm' in run_params.keys():
     # default values that should be the same for
