692595a847a577898d8aae0fc32089169f28add0
Matthew Creamer <matthew.s.creamer@gmail.com>
2023-08-20 11:41:17 -0400

    5th times the charm

--- run_inference.py
+++ run_inference.py
@@ -213,7 +213,7 @@ def infer_posterior(param_name, data_folder):
     # cpu_id 0 is the parent node which will send out the data to the children nodes
     if cpu_id == 0:
         if 'data_folder' in run_params.keys():
-            data_folder = run_params['model_folder']
+            data_folder = run_params['data_folder']
         data_folder = Path(data_folder)
         model_path = data_folder / 'models' / 'model_trained.pkl'
         data_train_path = data_folder / 'data_train.pkl'
