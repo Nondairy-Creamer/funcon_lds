c18f90fb67af626f90f772abec5072ad6e135d85
Matt Creamer <matthew.s.creamer@gmail.com>
2023-08-29 19:29:11 -0400

    Updated path to model for load_and_plot

--- load_and_plot.py
+++ load_and_plot.py
@@ -29,7 +29,7 @@ else:
 
     model_folder = Path(max_dir)
 
-model_trained_path = model_folder / 'model_trained.pkl'
+model_trained_path = model_folder / 'models' / 'model_trained.pkl'
 model_trained_file = open(model_trained_path, 'rb')
 model_trained = pickle.load(model_trained_file)
 model_trained_file.close()
